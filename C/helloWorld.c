7c7872972f254fd8d014a7db442939f0e2bf1648
ChampionZA <blunderbust44@gmail.com>
2021-07-02 10:02:12 +0200

    Starting with c

---
+++ C/helloWorld.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    
+    int size;
+    printf("How big is u're juicer: ");
+    scanf("%d", &size);
+    printf("Your jucier is %d digits big!!!", size);
+
+    return 0;
+}
