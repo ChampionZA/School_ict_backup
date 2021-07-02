2c0043020c4de106f1e4638d11101a9f06970f77
ChampionZA <blunderbust44@gmail.com>
2021-07-02 14:39:53 +0200

    udpate

--- C/helloWorld.c
+++ C/helloWorld.c
@@ -1,12 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int big(int num1, int num2, int num3) {
+    if (num1 >= num2 && num1 >= num3) {
+        return num1;
+    } else if (num2 >= num1 && num2 >= num3) {
+        return num2;
+    } else {
+        return num3;
+    }
+}
+
 int main() {
     
-    int size;
-    printf("How big is u're juicer: ");
-    scanf("%d", &size);
-    printf("Your jucier is %d digits big!!!", size);
+    int usrNumEntry1;
+    int usrNumEntry2;
+    int usrNumEntry3;
+
+    printf("Please enter two numbers separated by a space: ");
+    scanf("%d%d%d", &usrNumEntry1, &usrNumEntry2, &usrNumEntry3);
+
+    printf("Answer: %d", big(usrNumEntry1, usrNumEntry2, usrNumEntry3));
 
     return 0;
 }
