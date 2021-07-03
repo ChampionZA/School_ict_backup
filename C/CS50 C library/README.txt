2a0b0db5017643b3f6be4d3eaca7b0b2341c86ab
ChampionZA <57207172+ChampionZA@users.noreply.github.com>
2021-07-03 12:08:54 +0200

    udpate, and cs50 library

---
+++ C/CS50 C library/README.txt
@@ -0,0 +1,5 @@
+This is the library used in harvards cs50 course
+
+you can use it by adding both files to your mingw include folder and using
+
+#include <cs50.c> in your C code.
--- C/Harvards's_CS50_notes.c
+++ C/Harvards's_CS50_notes.c
@@ -1,12 +1,17 @@
+#include <cs50.c>
 #include <stdlib.h>
 #include <stdio.h>
 
 int main(void) 
 {
-    // Getting string input from a user 
-    char answer[20] = get_string("What is your name?\n");
-    printf("Hello, %s", answer);
+    /*
+    int x = get_int("Num1: ");
+    float y = get_float("Num2: ");
 
-    printf("Hello, world!");
+    printf("x/y: %.7f", x/y);
+    //             ^ the amount of decimal places   */
 
+    // never comare floating point numbers due to precision errors
+
+    
 }
