0535662220ebb9fb91a647da62972f63fbea4b0a
ChampionZA <57207172+ChampionZA@users.noreply.github.com>
2021-07-03 16:37:33 +0200

    update

--- C/Harvards's_CS50_notes.c
+++ C/Harvards's_CS50_notes.c
@@ -1,11 +1,9 @@
 #include <cs50.c>
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 
-void cough(void)
-{
-    printf("cough\n");
-}
+int amountOfScoreEntrys;
 
 int main(void) 
 {
@@ -22,5 +20,25 @@ int main(void)
 
         // A float takes up about 32 bits of data on a normal computer but if you want twice as many bits then you can use a double!!;
 
+        // variables exist inside of scopes (inside curly braces)
+
+        // for style keep the main function at the top of the code and reference other functions at the top of the code just to it knows it exists.
+
     
+        // how to get the len of a string by using strlen and by including the <string.h> header
+
+        // a string is an array that contains individual characters. you know where the string ends in memory by using a null byte
+        // aka (\0)
+
+        //by using casting you can store one data type as another data type | int c = (int) s[i]
+
+        //to capatilise and lower letters you can use the <ctype.h> library
+
+        //in argc, string argv[]
+        //     ^ amount of characters entered in main 
+        //                  ^ words entered in main
+
+    
+      
+
 }
