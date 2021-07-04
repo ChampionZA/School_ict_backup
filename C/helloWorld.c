097711aba71d0d8fb6c3302817e56634a61f74ee
ChampionZA <blunderbust44@gmail.com>
2021-07-04 12:20:09 +0200

    update

--- C/helloWorld.c
+++ C/helloWorld.c
@@ -1,24 +1,60 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <cs50.c>
 
-int main() {
+int squareNum(int num);
 
-    int unknownNum = 7;
-    int usrGuess;
-    int attemptNum = 0;
+int main(void)
+{
+    int total = 0;
+    int amountOfNumsWithinUsrInput;
+    int usrNumInput;
+    while (true)
+    {
+        usrNumInput = get_int("Please enter your number: ");
 
-    while (1) {
-        if (usrGuess == unknownNum) {
-            printf("You have won the game GG");
-            break;
-        } else if (attemptNum >= 3) {
-            printf("You have lost the game lol get good");
+        if (usrNumInput > 999)
+        {
+            printf("Number entered is too big");
+        } 
+        else if (usrNumInput < 0)
+        {
+            printf("Number entered too small");
+        }
+        else if (usrNumInput < 10)
+        {
+            amountOfNumsWithinUsrInput = 1;
             break;
         }
-        printf("Please enter you number: ");
-        scanf("%d", &usrGuess);
-        attemptNum++;
+        else if (usrNumInput < 100)
+        {
+            amountOfNumsWithinUsrInput = 2;
+            break;
+        }
+        else if (usrNumInput < 1000)
+        {
+            amountOfNumsWithinUsrInput = 3;
+            break;
+        }
     }
 
-    return 0;
+    string n = (string) usrNumInput;
+    for (int i = 0; i < amountOfNumsWithinUsrInput; i++)
+    {
+        int y = (int) n[i];
+        total += squareNum(y);
+    }
+
+    if (total == usrNumInput)
+    {
+        printf("Entered number is an armstrong number");
+    } else 
+    {
+        printf("Entered number is not an armstrong number");
+    }
+}
+
+int squareNum(int num)
+{
+    return num*num*num;
 }
--- CurrentlyWorkingOn/main.py
+++ CurrentlyWorkingOn/main.py
@@ -1,60 +1,20 @@
-studentNames = []
-studentMarksTest1 = []
-studentMarksTest2 = []
-studentMarksTest3 = []
-totalScoreForEveryStudent = []
+from typing import MutableMapping
+import numba
 
-amountOfStudents = 2
+def squareNum(num):
+    return num*num*num
 
-tempHighestTotalScore = 0
-indexOfHighestScoreHoldersName = None
-indexCounter = -1
+usrInputedNumber = int(input("Enter your number: "))
 
-for x in range(amountOfStudents):
-    inputName = input('Enter the name of a child: ')
-    studentNames.append(inputName)
-    while True:
-        inputTest1 = int(input(f"Enter {inputName}'s mark for Test 1: "))
-        if inputTest1 > 20:
-            print('Error: Mark score too high')
-        elif inputTest1 < 0:
-            print('Error: Less than zero score')
-        else:
-            break
-    studentMarksTest1.append(inputTest1)
-    while True:
-        inputTest2 = int(input(f"Enter {inputName}'s mark for Test 2: "))
-        if inputTest2 > 25:
-            print('Error: Mark score too high')
-        elif inputTest2 < 0:
-            print('Error: Less than zero score')
-        else:
-            break
-    studentMarksTest2.append(inputTest2)
-    while True:
-        inputTest3 = int(input(f"Enter {inputName}'s mark for Test 3: "))
-        if inputTest3 > 35:
-            print('Error: Mark score too high')
-        elif inputTest3 < 0:
-            print('Error: Less than zero score')
-        else:
-            break
-    studentMarksTest3.append(inputTest3)
-for x in range(amountOfStudents):
-    tempTotal = studentMarksTest1[x] + studentMarksTest2[x] + studentMarksTest3[x]
-    print(f"NAME: {studentNames[x]} - SCORE: {tempTotal}")
-    totalScoreForEveryStudent.append(tempTotal)
-tempTotal = 0
-for x in totalScoreForEveryStudent:
-    tempTotal += x 
-print(f"The average total score for the whole class is {tempTotal/amountOfStudents}")
+total = None
+temp = str(usrInputedNumber)
 
-for x in totalScoreForEveryStudent:
-    indexCounter += 1 
-    if x > tempHighestTotalScore:
-        tempHighestTotalScore = x
-        indexOfHighestScoreHoldersName = indexCounter
-    else:
-        pass
+for i in len(temp):
+    temp = str(usrInputedNumber)
+    temp = temp[i]
+    total += squareNum[int(temp)]
 
-print(f"The student with the highest total score is {studentNames[indexOfHighestScoreHoldersName]}, with a score of {tempHighestTotalScore}")
+if (total == usrInputedNumber):
+    print("That number is an armstrong number")
+else:
+    print("That is not an armstrong number")
